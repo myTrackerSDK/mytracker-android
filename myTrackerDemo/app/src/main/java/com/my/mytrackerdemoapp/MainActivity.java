e9b845e752a68adb205718352188d3f4acc9597b
Alexander Kobozev <eternalkaif@gmail.com>
2016-09-22 17:55:28 +0300

    Update lib version to 1.3.6

--- myTrackerDemo/app/src/main/java/com/my/mytrackerdemoapp/MainActivity.java
+++ myTrackerDemo/app/src/main/java/com/my/mytrackerdemoapp/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		MyTracker.getTrackerParams().setTrackingEnvironmentEnabled(true);
 	}
 
 	@Override
